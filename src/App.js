866dab985f00792a77b20728b93239a00efcda0d
qortpdms2 <qortpdms2@kyungsung.ac.kr>
2023-07-03 20:19:45 +0900

    1-22강

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  let posts = '강남 고기 맛집';
+  function fun(){
+    return 100;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+          <div>개발 Blog</div>
+      </div>
+      <h4 className="content">{ fun() }</h4>
     </div>
   );
 }
